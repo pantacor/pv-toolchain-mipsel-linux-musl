20f778343b57e48640e75245b6c67e9b78434609
gaurav-git <gauravpathak129@gmail.com>
2020-08-25 10:05:26 +0530

    Adding musl libc based gcc toolchain for LE MIPS Architecture

---
+++ mipsel-linux-musl/lib/gcc/mipsel-linux-musl/6.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../src_toolchain/configure --enable-languages=c,c++ --with-float=soft --with-arch=mips32 --disable-werror --target=mipsel-linux-musl --prefix= --libdir=/lib --disable-multilib --with-sysroot=/mipsel-linux-musl --enable-tls --disable-libmudflap --disable-libsanitizer --disable-gnu-indirect-function --disable-libmpx --enable-deterministic-archives --enable-libstdcxx-time --with-build-sysroot=/build/build/local/mipsel-linux-musl/obj_sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "mips32" }, { "float", "soft" }, { "llsc", "llsc" } };
